ff4e41e2dd27cdb436b50147d1262dcdcad4a318
pasqualedecicco157-debug <pasqualedecicco157@gmail.com>
2026-01-03 15:48:02 +0000

    modifica main

--- com/storyfinder/AvvioGioco.java
+++ com/storyfinder/AvvioGioco.java
@@ -4,3 +4,9 @@ public class AvvioGioco {
 
     public static void main() {}
 }
+  Scanner scanner = new Scanner(System.in);
+  System.out.println("Che cosa vuoi cercare?");
+  String argomento = scanner.nextline();
+  GestoreStoria gestorestoria = new Gestorestoria();
+  Storia storia = gestoreStoria.getBestStory();
+  System.out.println(storia);
