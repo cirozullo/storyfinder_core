debafa5d5458af13ed8380185a544549ef52b47b
Ciro Zullo <211477495+cirozullo@users.noreply.github.com>
2026-01-04 15:59:46 +0100

    Aggiunto altre storie

--- com/storyfinder/AvvioGioco.java
+++ com/storyfinder/AvvioGioco.java
@@ -6,19 +6,24 @@ public class AvvioGioco {
 
     public static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("scrivi un nome");
+        System.out.println("Scrivi un nome per il tuo personaggio: ");
         String nome = scanner.nextLine();
-        System.out.println("scrivi un luogo");
+        System.out.println("Scrivi il luogo dove viene ambientata la storia: ");
         String luogo = scanner.nextLine();
-        System.out.println("Che cosa vuoi cercare?");
+        System.out.println(
+            "Indica l'oggetto che il tuo personaggio utilizzerà: "
+        );
+        String oggetto = scanner.nextLine();
+        System.out.println("Indica una parola chiave della tua storia: ");
         String argomento = scanner.nextLine();
         GestoreStoria gestoreStoria = new GestoreStoria();
         Storia storia = gestoreStoria.getBestStory(argomento);
         String testo = storia.getTesto_storia();
         String storiaFinale = testo
             .replace("[nome]", nome)
-            .replace("[luogo]", luogo);
-        System.out.println(storiaFinale);
+            .replace("[luogo]", luogo)
+            .replace("[oggetto]", oggetto);
+        System.out.println("\n" + storiaFinale);
         scanner.close();
     }
 }
